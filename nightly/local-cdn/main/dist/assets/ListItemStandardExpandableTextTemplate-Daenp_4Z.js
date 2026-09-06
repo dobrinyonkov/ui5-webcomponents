c3076bb0ffe6c93f8a972645a877b4f9a0d83fbc
dobrinyonkov <dobrinyonkov@users.noreply.github.com>
2026-09-06 02:16:29 +0000

    Deploying to gh-pages from @ dobrinyonkov/webcomponents@6312c9df02331b7fa3df95838af5f1dcb36c2345 🚀

---
+++ nightly/local-cdn/main/dist/assets/ListItemStandardExpandableTextTemplate-Daenp_4Z.js
@@ -0,0 +1,1 @@
+import{a as x,E as m}from"./bundle.esm-CiUM_4SI.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
